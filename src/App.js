9629235458397638af4d393ba7e1eb04da370855
ana-225 <ana.vidal.s@hotmail.com>
2021-09-16 10:02:47 -0300

    Login view is created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+// import Header from './components/Header.jsx';
+import Login from './componentes/Login';
+// import './App.css'
+// import Waiter from './components/Waiter.jsx';
+// import Drinks from './components/Drinks.jsx';
+// import HeaderKitchen from './components/HeaderKitchen';
+// import Kitchen from './components/Kitchen';
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+     <div className='background'>
+       <Switch>
+        <Route path ='/' exact>
+          <Login/>
+        </Route>
+        {/* <Route path = '/lunch'>
+          <Header />
+          <div className='waiter-container'>
+          <Waiter />
+          </div>
+        </Route>
+        <Route path = '/drinks'>
+          <Header />
+          <Drinks />
+        </Route>
+        <Route path = '/kitchen'>
+          <HeaderKitchen />
+          <Kitchen />
+        // </Route> */}
+    </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/componentes/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+// import { auth } from '../firebaseconfig'
+import { Form, Button } from 'react-bootstrap'
+// import logo from '../img/logo.png'
+import { useHistory}  from 'react-router-dom'
+
+
+const Login = () => {
+  const historial = useHistory('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [msgerror, setMsgError] = useState(null);
+  const style = {
+    color: "red"
+  }
+
+  // const LoginWaiter = e => {
+  //   console.log(e.target.innerText);
+
+  //   auth.signInWithEmailAndPassword(email, pass)
+  //   .then((r) => {
+  //     e.target.innerText === "Mesero" ? historial.push('/lunch') : historial.push('/kitchen')
+  //   })
+  //   .catch((err) => {
+  //     const { code } = err;
+  //     switch(code){
+  //       case "auth/invalid-email":
+  //         setMsgError("Formato de correo invalido");
+  //         break;
+  //       case "auth/user-not-found":
+  //         setMsgError("Usuario no encontrado");
+  //         break;
+  //       default: setMsgError("");
+  //     }
+  //   })
+  // }
+
+  return ( 
+    <div className = 'login-container'>
+      
+      <div>
+        <Form >
+          <Form.Group  controlId="formBasicEmail">
+            <Form.Label></Form.Label>
+            <Form.Control
+            onChange = {(e) => {setEmail(e.target.value)}}
+            type="email" placeholder="Usuario" />
+            <Form.Text className="text-muted">
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label></Form.Label>
+            <Form.Control 
+            onChange = {(e) => {setPass(e.target.value)}}
+            type="password" placeholder="Contraseña" />
+          </Form.Group>
+      </Form>
+        <div>  
+          <Button 
+              // onClick={(e) => LoginWaiter(e)}
+              // variant="primary"
+              // type="submit"
+            >
+              Ingresar
+          </Button>
+        </div>
+          { msgerror && <div style={style}> {msgerror} </div> }
+      </div>
+    </div>
+  )
+}
+
+export default Login;
