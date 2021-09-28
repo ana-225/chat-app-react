9ad1ff8450c0b0b7ec796c096e1c010929217a5b
CarolAbcl <carolallende@gmail.com>
2021-09-28 12:43:30 -0300

    conection with backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ConversationsList from "./containers/ConversationsList";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+    };
+  }
+
+  generateUid() {
+    return new Date().getTime() + "" + Math.floor(Math.random() * 7 + 4);
+  }
+
+  handleChange(e) {
+    this.setState({ username: e.target.value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.handleLogin();
+  }
+  handleKeyPress(e) {
+    if (e.key === "Enter") {
+      this.handleLogin();
+    }
+    return false;
+  }
+
+  handleLogin() {
+    let username = this.state.username;
+    const uid = this.generateUid();
+    if (!username) {
+      username = "Guest";
+    }
+    this.setState({ uid: uid, username: `${username} ID:${uid}` });
+  }
+  render() {
+    let renderDOM;
+    if (this.state.uid) {
+      renderDOM = (
+        <ConversationsList
+          username={this.state.username}
+          uid={this.state.uid}
+        />
+      );
+    } else {
+      renderDOM = (
+        <Container component="main" maxWidth="xs">
+          <div className="loginForm">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              autoFocus
+              placeholder="Please input a screen name to continue"
+              onChange={this.handleChange.bind(this)}
+              onKeyPress={this.handleKeyPress.bind(this)}
+            />
+            <div className="submit">
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={this.handleClick.bind(this)}
+              >
+                Submit
+              </Button>
+            </div>
+          </div>
+        </Container>
+      );
+    }
+    return <div>{renderDOM}</div>;
+  }
 }
-
-export default App;
