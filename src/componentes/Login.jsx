49f66733203f60ff93397c25fa3ad07ab8cae8f4
CarolAbcl <carolallende@gmail.com>
2021-10-11 08:54:21 -0300

    register and login working

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -8,14 +8,16 @@ import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Link from '@material-ui/core/Link'
 import { useHistory } from 'react-router-dom';
+import '../App.css';
 
 
 
 const Login = () => {
   localStorage.clear();
   const history = useHistory();
-  const [email, setEmail] = useState('')
-  const [pass, setPass] = useState('')
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [name, setName] = useState('');
   const [error, setError] = useState(null)
   const [isRegister, setIsRegister] = useState(true)
   
@@ -31,33 +33,61 @@ const Login = () => {
         return
     }
 
+  if (isRegister){
+    console.log('ya registrado')
+    fetch('https://chat-app-comes.herokuapp.com/auth', {
+      method: 'post',
+      headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify( {
 
-  fetch('https://chat-app-comes.herokuapp.com/auth', {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify( {
-
-            "email": email,
-            "password": pass,
-        })
-      
-    })
-    .then((res) => res.json())
-    .then((res) => {
-        console.log(res);
-        const response = res.message;
-        if (response) {
-          console.log('usuario no logeado');
-        } else {
-          localStorage.token = res.token;
-          console.log(localStorage.token); 
-          return history.push("/chat");
-        }
-    })
-    .catch(console.log)
+          "email": email,
+          "password": pass,
+      })
+    
+  })
+  .then((res) => res.json())
+  .then((res) => {
+      console.log(res);
+      const response = res.message;
+      if (response) {
+        console.log('usuario no logeado');
+      } else {
+        localStorage.token = res.token;
+        console.log(localStorage.token); 
+        return history.push("/chat");
+      }
+  })
+  .catch(console.log)
+  } else {
+    console.log('nuevo usuario');
+    fetch('https://chat-app-comes.herokuapp.com/users', {
+      method: 'post',
+      headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify( {
+          "name": name,
+          "email": email,
+          "password": pass,
+      })
+    
+  })
+  // .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+    if (res.status === 200) {
+      alert('Usuario Registrado exitosamente, por favor inicie sesión');
+    } else {
+      alert ('El usuario ya esta registrado');
+    }
+  })
+  .catch(console.log)
+  } 
+  
   }
   return ( 
       <Grid
@@ -72,7 +102,16 @@ const Login = () => {
                 }
               <Box 
               textAlign="center">
-                
+              <div className = {isRegister ? 'userName' : ''}>
+               <TextField
+                 type="text"
+                 className="name"
+                 placeholder="Nombre Usuario"
+                 onChange={e => setName(e.target.value)}
+                 value={name}  
+               />
+              </div>
+              <br/>
               <TextField
                 type="email"
                 className="email"
@@ -118,7 +157,9 @@ const Login = () => {
               type="button"
               variant="body2"
               onClick={() => {
-                setIsRegister(!isRegister)}}
+                setIsRegister(!isRegister)
+                  
+              }}
             >
               {
                 isRegister ? '¿Eres nuevo?' : '¿Ya tienes cuenta?'
